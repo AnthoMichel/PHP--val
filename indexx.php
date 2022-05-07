94241d6ede033dd3750277f2af10cfe7b1a78a55
Anthony Fresnel <100720509+AnthoMichel@users.noreply.github.com>
2022-05-07 10:25:20 +0200

    header + footer

---
+++ indexx.php
@@ -0,0 +1,23 @@
+<?php require_once "./partials/header.php"?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wf-immo</title>
+</head>
+<body>
+    
+
+     <main><!--Tableau avec les apportements de la base de donnée 
+     rapporter avec require once a card sur un fichier partials-->
+    
+    </main> 
+
+
+     
+     <?php require_once "./partials/footer.php" ?>
+</body>
+</html>
