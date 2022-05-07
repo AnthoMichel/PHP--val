f604a82cbc5eb7211d5a8159d4c499b5146dd0bd
Anthony Fresnel <100720509+AnthoMichel@users.noreply.github.com>
2022-05-07 11:06:25 +0200

    foreach tableau avec photo

--- partials/page.test.php
+++ partials/page.test.php
@@ -9,6 +9,40 @@ $req->closeCursor();
 ?>
 
 <?php
-foreach ($apparts as $appart) {
-    var_dump($appart) ;
+foreach ($apparts as $appart) :?>
+<div class="card text-center shadow my-3">
+    
+        <div class="card-header bg-dark text-white ">
+            <h2 class="card-title"><?= ucwords($appart["titre"]) ?></h2>
+        </div>
+    
+        <div class="card-body">
+            <img src="<?= $appart["photo"] ?>" alt="photo1"> 
+            <hr>
+            <p>Adresse: 
+                <?php if ($appart["adresse"]): ?>
+                <span><?= mb_strtoupper($appart["adresse"])?></span>
+                    <?php else: ?>
+                        <span>NC</span>
+                <?php endif; ?>
+    
+            </p>
+    
+            <p>Ville: <?php ($appart["ville"])?></p>
+    
+            <p>CP :
+                <?php if ($appart["cp"]): ?>
+                    <span><?= $appart["cp"]?></span>
+                    <?php else: ?>
+                        <span>NC</span>
+                <?php endif; ?>
+            </p>
+    
+            <p>Réserver maintenant !</p>
+            <button  class="btn btn-primary ">Réserver</button>
+        </div>
+    </div>
+    <hr>
+    
 }
+<?php endforeach ?>
