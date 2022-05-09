dbd69205e60dc80b675f4d58b481aba493dbc3b8
Anthony Fresnel <100720509+AnthoMichel@users.noreply.github.com>
2022-05-09 18:51:05 +0200

    correction fichier sql

--- sql/immo.sql
+++ sql/immo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 09 mai 2022 à 16:14
+-- Généré le : lun. 09 mai 2022 à 16:49
 -- Version du serveur : 5.7.36
 -- Version de PHP : 8.0.13
 
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `logement` (
 --
 
 INSERT INTO `logement` (`id_logement`, `titre`, `adresse`, `ville`, `cp`, `surface`, `prix`, `photo`, `type`, `description`) VALUES
-(1, 'Bel appartement', '71 boulevard pourparle', 'Marseille', 20001, 1501, 2000001, './assets/img/image1.jpg', 'location', '1'),
+(1, 'Bel appartement', '71 boulevard pourparle', 'Marseill', 20001, 1501, 2000001, './assets/img/image1.jpg', 'location', '1'),
 (2, 'appart2', '78 boulevard voltaire', 'marseille', 13010, 150, 500000, './assets/img/image2.jpg', 'location', NULL),
 (8, 'caravane flambam neuf ', 'bord de route et autoroute', 'N\'importe ou', 66666, 10, 10000, 'assets/img/image3.jpg', 'vente', 'Magnifique caravane pour vivre n\'importe ou'),
 (9, 'magnifique tour eiffel', 'marché au puce', 'Marseille', 13009, 150, 15500000, 'assets/img/image4.jpg', 'vente', NULL),
